540592f8b6a581de3c3f9c06580c1e057bef0b44
Bhanu1132 <kb7097@gmail.com>
2023-03-23 18:42:58 +0530

    first commit

---
+++ src/com/prime/Prime.java
@@ -0,0 +1,30 @@
+package com.prime;
+import java.util.Scanner;
+public class Prime {
+	public static void Prime(int range)
+	{
+		System.out.print("Prime Numbers are: ");
+		int count;
+		for(int i=2; i<=range; i++)
+		{
+			count=0;
+			for(int j=1; j<=i; j++)
+			{
+				if(i%j==0)
+				{
+					count++;
+				}
+			}
+			if(count==2)
+			{
+				System.out.print(i + " ");
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the range of prime numbers: ");
+		int range = sc.nextInt();
+		Prime(range);
+	}
+}
